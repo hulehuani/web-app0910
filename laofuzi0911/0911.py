0b57d31b40db834753aa598e359a399e6164b2ca
admin <www.523258779.com>
2020-09-11 22:53:11 +0800

    添加自动发送邮件功能 --- hu

---
+++ laofuzi0911/0911.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time: 2020/9/11 22:48
+# @Author : huahua
+# @Email : 523258779@qq.com
+# @File :  0911.py
---
+++ laofuzi0911/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time: 2020/9/11 22:47
+# @Author : huahua
+# @Email : 523258779@qq.com
+# @File :  __init__.py.py
